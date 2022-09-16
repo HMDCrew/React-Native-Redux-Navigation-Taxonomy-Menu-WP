1287ea06dc1c5b0c7efbd8a819c853cb74411fd5
Andrei L <andrei.leca55@gmail.com>
2022-09-16 18:39:45 +0200

    Removed unused screen

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -6,9 +6,6 @@ import { COLORS } from '../constants/style';
 
 
 import Home from '../screens/Home'
-import Details from '../screens/Details'
-import Profile from '../screens/Profile'
-import Settings from '../screens/Settings'
 
 import Stack from './Stack';
 
@@ -29,9 +26,6 @@ export class DrawerNavigator extends Component {
             >
 
                 <Drawer.Screen name="Home" component={Home} />
-                <Drawer.Screen name="Details" component={Details} />
-                <Drawer.Screen name="Profile" component={Profile} />
-                <Drawer.Screen name="Settings" component={Settings} />
 
                 {/* don't use stack on first position because is used for nested store and product */}
                 <Drawer.Screen name="STACK" component={Stack} />
